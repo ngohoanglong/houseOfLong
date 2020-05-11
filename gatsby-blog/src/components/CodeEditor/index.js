9680cc875f31ac629d84a2b4f8ae5ab7f3bad90f
longnh01 <ngohoanglongptit@gmail.com>
2020-05-11 15:16:26 +0700

    update

---
+++ gatsby-blog/src/components/CodeEditor/index.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+import React from "react"
+import AceEditor from "react-ace"
+const languages = ["javascript"]
+const themes = ["monokai"]
+languages.forEach(lang => {
+  require(`ace-builds/src-noconflict/mode-${lang}`)
+  require(`ace-builds/src-noconflict/snippets/${lang}`)
+})
+themes.forEach(theme => require(`ace-builds/src-noconflict/theme-${theme}`))
+const CodeEditor = ({ value, onChange }) => {
+  return (
+    <AceEditor
+      width={"100%"}
+      height={"100%"}
+      mode="javascript"
+      theme="monokai"
+      onChange={onChange}
+      name="UNIQUE_ID_OF_DIV"
+      value={value}
+    />
+  )
+}
+export default CodeEditor
